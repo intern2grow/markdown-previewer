1bbef437c7fa6033c943e80238d200b4ef665dbd
Mabrouka <khalfamarouka@gmailcom>
2023-09-11 20:31:13 +0100

    dowing tasks

--- src/App.js
+++ src/App.js
@@ -1,48 +1,72 @@
-import './App.css';
-import React, {useState} from 'react';
-import {marked} from 'marked'
+import "./App.css";
+import React, { useState } from "react";
+import { marked } from "marked";
+import useLocalStorage from "./components/useLocalStorage";
+import Docs from "./components/Docs";
+
 
 const App = () => {
-  const [code, setCode] = useState('## Hello')
-  const [compiled, setCompiled] = useState('<h2 id="hello">Hello</h2>')
-  const [hide, hidePreview] = useState(true)
+  const [code, setCode] = useState("## Hello");
+  const [compiled, setCompiled] = useState('<h2 id="hello">Hello</h2>');
+  const [hide, hidePreview] = useState(true);
+  const [value, setValue] = useLocalStorage("myKey");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [activeContent, setActiveContent] = useState(null);
 
   const openMD = () => {
-    console.log(0)
-    hidePreview(true)
-  }
+    console.log(0);
+    hidePreview(true);
+  };
 
   const openPreview = () => {
-    console.log(0)
-    hidePreview(false)
-  }
+    console.log(0);
+    hidePreview(false);
+  };
+  const openModal = () => {
+    setActiveContent(null);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   const handleChange = (e) => {
-    setCode(e.target.value)
-    setCompiled(marked.parse(e.target.value))
-  }
+    setValue(e.target.value);
+    console.log(value);
+    setCode(value);
+    setCompiled(marked.parse(value));
+  };
 
   return (
     <>
       <h1>MarkDown Previewer React App</h1>
       <div className="container">
         <div className="btns">
-          <button onClick={openMD} className="btn">MarkDown</button>
-          <button onClick={openPreview}>Preview</button>
+          <button onClick={openMD} className="btn">
+            MarkDown
+          </button>
+          <button className="btn" onClick={openPreview}>Preview</button>
+          <button className="btn" onClick={openModal}>Docs</button>
         </div>
-        {
-        hide ? 
+         {isModalOpen && <Docs openModal={closeModal} />}
+       
+        {hide ? (
           <div>
-            <textarea onChange={handleChange} value={code}/>
-          </div> : 
-          <div>
-            <textarea value={compiled}/>
+            <textarea onChange={handleChange} value={code} />
           </div>
-        }
+        ) : (
+          <div>
+            <textarea value={compiled} />
+            <div>
+          <textarea value={activeContent || ""} readOnly />
+        </div>
+          </div>
+          
+        )}
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
